e6bf77e8d79a90267a1bbdcec9053ceb7e79f9d3
Anyanwu_Justice <105282903+Justin0207@users.noreply.github.com>
2024-10-22 22:43:58 +0100

    Update dropout_app.py

--- Week_6/dropout_app.py
+++ Week_6/dropout_app.py
@@ -49,7 +49,7 @@ def set_background(main_bg):
              background: url(data:image/{main_bg_ext};base64,{base64.b64encode(open(main_bg, "rb").read()).decode()});
              background-size: cover;
          }}
-         .st-emotion-cache-0 {{
+         .st-emotion-cache-1xw8zd {{
              background-color:black;
              }}
          </style>
